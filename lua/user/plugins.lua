9d4a2a1813025211f14e4537bc371f2a8049759e
kekshibata <kenergyintense@gmail.com>
2022-08-20 20:48:03 +0900

    installer to mason

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,8 +71,10 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  --  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
 
   use "vim-denops/denops.vim"
   use "Shougo/ddc.vim"
