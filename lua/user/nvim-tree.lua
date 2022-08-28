f66fd231f4338d749ae18ef36ae8dc78698f7408
kekshibata <kenergyintense@gmail.com>
2022-08-28 13:51:45 +0900

    rm: unused

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -16,12 +16,6 @@ nvim_tree.setup {
   hijack_directories = {
     enable = false,
   },
-  -- update_to_buf_dir = {
-  --   enable = false,
-  -- },
-  -- disable_netrw = true,
-  -- hijack_netrw = true,
-  -- open_on_setup = false,
   ignore_ft_on_setup = {
     "startify",
     "dashboard",
@@ -31,19 +25,7 @@ nvim_tree.setup {
     custom = { ".git" },
     exclude = { ".gitignore" },
   },
-  -- auto_close = true,
-  -- open_on_tab = false,
-  -- hijack_cursor = false,
   update_cwd = true,
-  -- update_to_buf_dir = {
-  --   enable = true,
-  --   auto_open = true,
-  -- },
-  -- --   error
-  -- --   info
-  -- --   question
-  -- --   warning
-  -- --   lightbulb
   renderer = {
     add_trailing = false,
     group_empty = false,
@@ -108,14 +90,6 @@ nvim_tree.setup {
     update_cwd = true,
     ignore_list = {},
   },
-  -- system_open = {
-  --   cmd = nil,
-  --   args = {},
-  -- },
-  -- filters = {
-  --   dotfiles = false,
-  --   custom = {},
-  -- },
   git = {
     enable = true,
     ignore = true,
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -17,27 +17,21 @@ configs.setup {
     disable = { "css", "markdown" }, -- list of language that will be disabled
     -- additional_vim_regex_highlighting = true,
   },
---  autopairs = {
---    enable = true,
---  },
   indent = { enable = true, disable = { "python", "css" } },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
   },
---  autotag = {
---    enable = true,
---    disable = { "xml" },
---  },
+    autotag = {
+      enable = true,
+      disable = { "xml" },
+    },
   rainbow = {
     enable = true,
     colors = {
       "Gold",
       "Orchid",
       "DodgerBlue",
-      -- "Cornsilk",
-      -- "Salmon",
-      -- "LawnGreen",
     },
     disable = { "html" },
   },
